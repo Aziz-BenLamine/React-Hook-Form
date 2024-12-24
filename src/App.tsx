292a4953febe47be6e974a172bc7bdd9d9013150
Aziz-BenLamine <azizbenlamine@gmail.com>
2024-12-24 15:23:17 +0100

    Init project

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import AdvancedForm from "./components/AdvancedForm";
+
+function App() {
+  return (
+    <div>
+      <AdvancedForm />
+    </div>
+  );
+}
+
+export default App;
